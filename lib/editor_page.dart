d271935ec92c9f6ad28e78061f53fece8d8f1d23
olaitan12 <olaitanaderoju@gmail.com>
2019-12-27 18:06:25 +0100

    Omolope

--- lib/editor_page.dart
+++ lib/editor_page.dart
@@ -3,20 +3,22 @@ import 'model/note_model.dart';
 import 'package:zefyr/zefyr.dart';
 
 class EditorPage extends StatefulWidget {
-  final Function add;
-  final Function update;
-  final int noteIndex;
-  final Note note;
   EditorPage({this.add, this.update, this.noteIndex, this.note});
+
+  final Function add;
+  final Note note;
+  final int noteIndex;
+  final Function update;
+
   @override
   _EditorPageState createState() => _EditorPageState();
 }
 class _EditorPageState extends State<EditorPage> {
-  ZefyrController _editorController;
-  TextEditingController _titleController;
-  /// Zefyr editor like any other input field requires a focus node.
-  FocusNode _focusNode;
   NotusDocument _document;
+  ZefyrController _editorController;
+  FocusNode _focusNode;
+  TextEditingController _titleController;
+
   @override
   void initState() {
     super.initState();
@@ -25,23 +27,81 @@ class _EditorPageState extends State<EditorPage> {
     _editorController = ZefyrController(_document);
     _focusNode = FocusNode();
   }
+
   NotusDocument _loadDocument() {
-    // If in edit mode then load the provided document
+
     return widget.note != null ? widget.note.document : NotusDocument();
   }
+
   TextEditingController _loadTitle() {
-    // If in edit mode then load the provided title
+
     return widget.note != null
         ? TextEditingController(text: widget.note.title)
         : TextEditingController();
   }
+
+  void _saveDocument(BuildContext context) {
+    final NotusDocument doc = _editorController.document;
+    final String title = _titleController.text;
+    final Note note = Note(title: title, document: doc);
+    // Check if we need to add new or edit old one
+    if (widget.noteIndex == null && widget.note == null) {
+      widget.add(note);
+    } else {
+      widget.update(widget.noteIndex, note);
+    }
+    Navigator.pop(context);
+  }
+
+  void _clearDocument(BuildContext context) async {
+    bool confirmed = await _getConfirmationDialog(context);
+    if (confirmed) {
+      _editorController.replaceText(
+          0, _editorController.document.length - 1, '');
+    }
+  }
+
+  Future<bool> _getConfirmationDialog(BuildContext context) async {
+    return showDialog<bool>(
+      context: context,
+      barrierDismissible:
+          false, // dialog is dismissible with a tap on the barrier
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm?'),
+          content: Row(
+            children: <Widget>[
+              Expanded(
+                child: Text('Are you sure you want to clear the contents?'),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
-    // To make the editor height responsive
+
     double editorHeight = screenHeight * 0.65;
     final editor = ZefyrField(
-      height: editorHeight, // set the editor's height
+      height: editorHeight, 
       controller: _editorController,
       focusNode: _focusNode,
       autofocus: false,
@@ -101,58 +161,6 @@ class _EditorPageState extends State<EditorPage> {
         ],
       ),
       body: ZefyrScaffold(child: form),
-    );
-  }
-  void _saveDocument(BuildContext context) {
-    final NotusDocument doc = _editorController.document;
-    final String title = _titleController.text;
-    final Note note = Note(title: title, document: doc);
-    // Check if we need to add new or edit old one
-    if (widget.noteIndex == null && widget.note == null) {
-      widget.add(note);
-    } else {
-      widget.update(widget.noteIndex, note);
-    }
-    Navigator.pop(context);
-  }
-  void _clearDocument(BuildContext context) async {
-    bool confirmed = await _getConfirmationDialog(context);
-    if (confirmed) {
-      _editorController.replaceText(
-          0, _editorController.document.length - 1, '');
-    }
-  }
-  Future<bool> _getConfirmationDialog(BuildContext context) async {
-    return showDialog<bool>(
-      context: context,
-      barrierDismissible:
-          false, // dialog is dismissible with a tap on the barrier
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Confirm?'),
-          content: Row(
-            children: <Widget>[
-              Expanded(
-                child: Text('Are you sure you want to clear the contents?'),
-              )
-            ],
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-            ),
-            FlatButton(
-              child: Text('Yes'),
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 }
